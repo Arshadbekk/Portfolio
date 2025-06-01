83a773db607a42f8acc3e2307cf6416ad4f83e00
Arshadbekk <100797505+Arshadbekk@users.noreply.github.com>
2025-06-01 19:11:13 +0400

    fix base for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://github.com/Arshadbekk/portfolio.git",
+  base: "portfolio",
   plugins: [react()],
 });
